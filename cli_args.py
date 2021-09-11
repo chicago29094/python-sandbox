7b4cfdcaf42d58abf1b476f204fc33dc33e3a261
chicago29094 <harry.anastopoulos@telusys.net>
2021-09-11 15:55:56 -0500

    CLI tests

---
+++ cli_args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+print(f'{sys}')
+print(f'{sys.argv}')
+print(f'{len(sys.argv)}')
+
+for arg in sys.argv:
+    print(f'{arg}')
+
---
+++ test.py
@@ -0,0 +1,9 @@
+import sys
+
+print(f'{sys}')
+print(f'{sys.argv}')
+print(f'{sys.argv.length()}')
+
+for arg in sys.argv:
+    print(f'{arg}')
+
