82eaedefba98ce3ecdb511b9edb3780308b1f74c
chicago29094 <harry.anastopoulos@telusys.net>
2021-09-12 19:36:22 -0500

    call by reference or by value tests

---
+++ basic_functions_01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+
+
+def california(a, b, c):
+    print(a, b, c)
+    a=101
+    b=102
+    c=103
+    print(a, b, c)
+    a1=5
+    b1=6
+    c1=7
+
+a1=1
+b1=2
+c1=3
+
+print("Start", a1, b1, c1)
+california(a1, b1, c1)
+print("End", a1, b1, c1)
+
