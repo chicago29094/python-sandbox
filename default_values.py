fb526cea2eb77011cf5d01ee126ec127019dbc09
chicago29094 <harry.anastopoulos@telusys.net>
2021-09-14 15:56:00 -0500

    function definitions default values

---
+++ default_values.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def f(a, L=[]):
+    L.append(a)
+    return L
+
+print(f(1))
+print(f(2))
+print(f(3))
+
+def f2(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+print(f2(1))
+print(f2(2))
+print(f2(3))
+
